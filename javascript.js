0d52c7853580fad385d758eb982596b494fc29c0
idekwat <184945414+idekwat@users.noreply.github.com>
2025-01-20 19:32:57 +0800

    Added playRound and incrementing winner score

--- javascript.js
+++ javascript.js
@@ -4,31 +4,88 @@ console.log("Hello Wold")
 let computerScore = 0;
 let humanScore = 0;
 
+
+function playGame() {
+    playRound();
+}
+
+function playRound(humanChoice, computerChoice) {
+
+    console.log(humanChoice);
+    console.log(computerChoice);
+
+    if(humanChoice == computerChoice) {
+        console.log(humanChoice + " vs " + computerChoice + ". It's a tie!")
+    }
+    else {
+        switch(humanChoice) {
+            case("Rock"):
+            if(computerChoice = "Scissors") {
+                console.log(humanChoice + " beats " + computerChoice + ". Player wins!")
+                humanScore++;
+            }
+            else {
+                console.log(computerChoice + " beats " + humanChoice + ". Computer wins!")
+                computerScore++;
+            }
+            break;
+            case("Paper"):
+            if(computerChoice = "Rock") {
+                console.log(humanChoice + " beats " + computerChoice + ". Player wins!")
+                humanScore++;
+            }
+            else {
+                console.log(computerChoice + " beats " + humanChoice + ". Computer wins!")
+                computerScore++;
+            }
+            break;
+            case("Scissors"):
+            if(computerChoice = "Paper") {
+                console.log(humanChoice + " beats " + computerChoice + ". Player wins!")
+                humanScore++;
+            }
+            else {
+                console.log(computerChoice + " beats " + humanChoice + ". Computer wins!")
+                computerScore++;
+            }
+            break;
+        }
+    }
+    console.log("Player score: " + humanScore);
+    console.log("Computer score: " + computerScore);
+}
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection)
+
+
 function getComputerChoice(computerRPS) {
 
     let randomResult = Math.floor(Math.random() * 3);
     switch(randomResult) {
         case 0:
-            computerRPS = "rock";
+            computerRPS = "Rock";
             break;
         case 1:
-            computerRPS = "paper";
+            computerRPS = "Paper";
             break;
         case 2:
-            computerRPS = "scissors";
+            computerRPS = "Scissors";
             break;
     }
     return computerRPS;
 }
 
-console.log(getComputerChoice(""))
-
 function getHumanChoice(humanRPS) {
-    humanRPS = prompt("Rock, Paper, Scissors");
-    return humanRPS;
+    humanRPS = prompt("Rock, Paper, Scissors").toLowerCase();
+    let capitalize = humanRPS.charAt(0).toUpperCase();
+    let humanOutput = capitalize+humanRPS.slice(1);
+    if(humanOutput == "Scissor")
+        humanOutput = "Scissors";
+    if(humanOutput == "Rock" || humanOutput == "Paper" || humanOutput == "Scissors") {
+        return humanOutput;
+    }
+    else
+    console.log("Wrong Input!") 
 }
-
-console.log(getHumanChoice(""))
-
-console.log(computerScore);
-console.log(humanScore);
