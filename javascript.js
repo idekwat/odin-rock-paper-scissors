51712d10292b644e8eb42e5e8c1c7224ab1ab95b
idekwat <184945414+idekwat@users.noreply.github.com>
2025-01-20 17:33:03 +0800

    created logic for computer input

--- javascript.js
+++ javascript.js
@@ -1,6 +1,23 @@
 
 console.log("Hello Wold")
 
-function getComputerChoice() {
-    
+function getComputerChoice(computerRPS) {
+
+    let randomResult = Math.floor(Math.random() * 3);
+    switch(randomResult) {
+        case 0:
+            computerRPS = "Rock";
+            break;
+        case 1:
+            computerRPS = "Paper";
+            break;
+        case 2:
+            computerRPS = "Scissors";
+            break;
+    }
+    return computerRPS;
 }
+
+console.log(getComputerChoice(""))
+
+
