5e7cedf4542eb994107f794ae86115a159e1975c
idekwat <184945414+idekwat@users.noreply.github.com>
2026-09-15 12:29:40 +0800

    added function for getting computer choice, added function for prompting user input, added function for playing round and conditionals for every possible choices, added function for playing game

--- scripts.js
+++ scripts.js
@@ -1,1 +1,43 @@
-console.log("Hello World!");
+let humanScore = 0;
+let computerScore = 0;
+
+const humanChoice = getHumanChoice;
+const computerChoice = getComputerChoice;
+
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    let computerChoice = "";
+    if(randomNum <1) computerChoice = "Rock";
+    else if (randomNum >1) computerChoice = "Paper";
+    else computerChoice = "Scissors";
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    const humanChoice = prompt("Rock, Paper, Scissors");
+    return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    let humanChoiceFormat = humanChoice(0).toUpperCase + humanChoice.slice(1).toLowerCase();
+    if(humanChoiceFormat == "Rock" && computerChoice == "Paper" ||
+        humanChoiceFormat == "Paper" && computerChoice == "Scissors" ||
+        humanChoiceFormat == "Scissors" && computerChoice == "Rock") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoiceFormat}`);
+        computerScore++;
+    }else if(humanChoiceFormat == computerChoice) {
+        console.log(`It's a tie! You have both chosen ${humanChoiceFormat}`);
+    }
+    else {
+        console.log(`You win! ${humanChoiceFormat} beats ${computerChoice}`)
+        humanScore++;
+    }
+}
+
+function playGame() {
+    playRound(getHumanChoice, getComputerChoice);
+    playRound(getHumanChoice, getComputerChoice);
+    playRound(getHumanChoice, getComputerChoice);
+    playRound(getHumanChoice, getComputerChoice);
+    playRound(getHumanChoice, getComputerChoice);
+}
